73b8eb0dfb2395ab3d6c80766a695f32c202d8cb
yuche <i@yuche.me>
2024-06-11 14:54:04 +0800

    fix: bitcoin demo

--- examples/bitcoin-demo/vite.config.ts
+++ examples/bitcoin-demo/vite.config.ts
@@ -3,49 +3,20 @@ import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
-// import { nodePolyfills } from 'vite-plugin-node-polyfills'
-// import WindiCSS from 'vite-plugin-windicss';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [
     wasm(),
     topLevelAwait(),
     solidPlugin(),
-    // nodePolyfills()
+    nodePolyfills()
   ],
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
-    rollupOptions: {
-      plugins: [
-        rollupNodePolyFill(),
-        // inject({ Buffer: ['buffer/', 'Buffer'] }),
-      ] as any[],
-    },
-  },
-  resolve: {
-    alias: {
-      stream: 'stream-browserify',
-      crypto: 'crypto-browserify',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
-      // target: ['chrome60', 'firefox60', 'safari11', 'edge18'],
-    },
   },
 })
