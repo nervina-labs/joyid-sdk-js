d3e84b5c4712f9716f467627551814feddd33baf
James Brown <soylentblue@hotmail.co.uk>
2025-06-09 16:50:03 +0800

    Update

--- examples/evm-demo/api/test.ts
+++ examples/evm-demo/api/test.ts
@@ -8,6 +8,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   return res.status(200).json({
     message: 'Test route working',
     method: req.method,
-    url: req.url
+    url: req.url,
   })
-} 
+}
